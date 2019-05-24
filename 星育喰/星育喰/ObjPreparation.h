8a057af696d0fe1e70b4337d06c66280e4fbd6c3
脇田 <GA1@DEV1-21>
2019-05-24 16:22:43 +0900

    HPゲージ追加

--- 星育喰/星育喰/ObjPreparation.h
+++ 星育喰/星育喰/ObjPreparation.h
@@ -67,8 +67,8 @@ private:
 	bool  m_boss_emerge_staging_f;	//ボス惑星出現演出開始フラグ
 	bool  m_finalcheck_f;			//最終確認ウインドウ表示管理フラグ(true:表示 / false:非表示)
 
-	wchar_t m_detail_message[DETAIL_MES_MAX_FONT_LINE][13];	//詳細説明(敵惑星、スペシャル技)のフォント用
-	wchar_t m_Enemy_detail_message[3][5][DETAIL_MES_MAX_FONT_LINE][13];//各敵惑星の詳細説明文管理用([ステージ数][敵惑星ナンバー][最大フォント行数][文字データ格納可能数])
+	wchar_t m_detail_message[DETAIL_MES_MAX_FONT_LINE][100];	//詳細説明(敵惑星、スペシャル技)のフォント用
+	wchar_t m_Enemy_detail_message[3][5][DETAIL_MES_MAX_FONT_LINE][100];//各敵惑星の詳細説明文管理用([ステージ数][敵惑星ナンバー][最大フォント行数][文字データ格納可能数])
 	float	m_detail_message_window_top;	//詳細説明(敵惑星、スペシャル技)ウインドウの描画先表示位置(top)
 	float	m_detail_message_window_left;	//詳細説明(敵惑星、スペシャル技)ウインドウの描画先表示位置(left)
 	float	m_detail_message_window_right;	//詳細説明(敵惑星、スペシャル技)ウインドウの描画先表示位置(right)
