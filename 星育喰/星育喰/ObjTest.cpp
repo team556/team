b7af0ce1d22de8e1116553841ed37992cd85f035
後庵(家作業) <@>
2019-05-28 01:49:44 +0900

    Git機能テスト終了

--- 星育喰/星育喰/ObjTest.cpp
+++ 星育喰/星育喰/ObjTest.cpp
@@ -27,10 +27,6 @@ void CObjTest::Init()
 //アクション
 void CObjTest::Action()
 {
-	//testA
-	//testB
-	//testC
-
 	////マウスの位置を取得
 	//m_mou_x = (float)Input::GetPosX();
 	//m_mou_y = (float)Input::GetPosY();
