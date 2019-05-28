73c51639b32db75ad3b5d13d4e73ba2d4d65dfdf
脇田 <GA1@DEV1-21>
2019-05-29 00:38:30 +0900

    コメント付け忘れ

--- 星育喰/星育喰/ObjRocketButton.cpp
+++ 星育喰/星育喰/ObjRocketButton.cpp
@@ -16,6 +16,7 @@ using namespace GameL;
 #define RECAST_COMPLETE_TIME (60.0f * g_Recast_time)//ミサイルのリキャスト完了タイム
 #define RECAST_COMPLETE_POD_TIME (60.0f * 6)//ポッドのリキャスト完了タイム
 
+//リキャストタイム用のグローバル変数定義
 int g_Recast_time;
 
 //コンストラクタ
