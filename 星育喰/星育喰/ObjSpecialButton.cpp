70493546ad49acb46dd7a8e53fc9114af3425982
後庵 <GA2B@DEV3-4>
2019-06-24 15:53:16 +0900

    スペシャル技Sキーのプログラム部分、少々修正。

--- 星育喰/星育喰/ObjSpecialButton.cpp
+++ 星育喰/星育喰/ObjSpecialButton.cpp
@@ -105,6 +105,14 @@ void CObjSpecialButton::Action()
 	m_mou_r = Input::GetMouButtonR();
 	m_mou_l = Input::GetMouButtonL();
 
+	//▼プレイヤー惑星スペシャル技処理(Sキー)
+	if ((Input::GetVKey('S')) &&						//Sキー
+		(g_Special_equipment != 0) &&					//スペシャル技装備してるかチェック
+		(m_is_used_special[PLAYER] == false))			//スペシャル技が未使用であれば実行
+	{
+		m_is_invocating[PLAYER] = true;		//発動中管理フラグON
+	}
+
 	//▼プレイヤー惑星スペシャル技処理
 	if ((m_x <= m_mou_x && m_mou_x <= (m_x + m_w)) && 	//X軸範囲
 		(m_y <= m_mou_y && m_mou_y <= (m_y + m_h)) &&	//Y軸範囲
@@ -120,21 +128,6 @@ void CObjSpecialButton::Action()
 			Special_process(PLAYER, ENEMY, g_Special_equipment);	//スペシャル技処理関数を呼び出す
 			m_a = 0.3f;		//スペシャル技ボタンを透明化
 		}
-	}
-
-	//▼プレイヤー惑星スペシャル技処理(Sキー)
-	if( (Input::GetVKey('S')) &&
-		(g_Special_equipment != 0) &&					//スペシャル技装備してるかチェック
-		(m_is_used_special[PLAYER] == false) ||			//スペシャル技が未使用であれば実行
-		(m_is_invocating[PLAYER] == true))				//また、現在スペシャル技発動中であっても実行
-	{
-	//エネミーがスペシャル技発動演出中である場合、
-	//以下の処理を実行しないようにする。
-	if (m_special_staging_f[ENEMY] == false)
-	{
-		Special_process(PLAYER, ENEMY, g_Special_equipment);	//スペシャル技処理関数を呼び出す
-		m_a = 0.3f;		//スペシャル技ボタンを透明化
-	}
 	}
 
 	//▼敵惑星スペシャル技処理
