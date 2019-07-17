16bf7a596556bffeab52f3a997894c2bab8ee92e
池田 <GA1@DEV1-15>
2019-07-17 12:32:04 +0900

    倉庫選択ボタン画像変更

--- 星育喰/星育喰/SceneTraining.cpp
+++ 星育喰/星育喰/SceneTraining.cpp
@@ -169,19 +169,19 @@ void CSceneTraining::InitScene()
 	//倉庫画像読み込み27番に登録
 	Draw::LoadImage(L"倉庫.png", 27, TEX_SIZE_512);
 
-	//資材選択ボタン画像読み込み28番に登録
-	Draw::LoadImage(L"資材選択ボタン.png", 28 , TEX_SIZE_512);
+	//資材ボタン画像読み込み28番に登録
+	Draw::LoadImage(L"資材ボタン.png", 28 , TEX_SIZE_512);
 
-	//住民選択ボタン画像読み込み29番に登録
-	Draw::LoadImage(L"住民選択ボタン.png", 29, TEX_SIZE_512);
+	//住民ボタン画像読み込み29番に登録
+	Draw::LoadImage(L"住民ボタン.png", 29, TEX_SIZE_512);
 
-	//スペシャル技選択ボタン画像読み込み30番に登録
-	Draw::LoadImage(L"スペシャル技選択ボタン.png", 30, TEX_SIZE_512);
+	//スペシャル技ボタン画像読み込み30番に登録
+	Draw::LoadImage(L"スペシャルボタン.png", 30, TEX_SIZE_512);
 
-	//装備選択ボタン画像読み込み31番に登録
-	Draw::LoadImage(L"装備選択ボタン.png", 31, TEX_SIZE_512);
+	//装備ボタン画像読み込み31番に登録
+	Draw::LoadImage(L"装備ボタン.png", 31, TEX_SIZE_512);
 
-	//資材ボタン選択後の画面背景画像32番に登録
+	//資材ボタン後の画面背景画像32番に登録
 	Draw::LoadImage(L"w.png", 32 , TEX_SIZE_512);
 
 	//資材(木材)画像33番に登録
