677b849e1afc2c22b9dd6a887ef6945b58fee4ed
GA2B <GA2B@DEV3-14>
2019-09-02 16:10:46 +0900

    クリア画面で資材の獲得数をfontdrawで表示するようにした

--- 星育喰/星育喰/ObjFightClear.cpp
+++ 星育喰/星育喰/ObjFightClear.cpp
@@ -840,30 +840,31 @@ void CObjFightClear::Draw()
 				dst.m_bottom = FONT_Y + 190.0f;
 				Draw::Draw(41, &src, &dst, c[4], 0.0f);
 
-				//十の位
-				src.m_top = 1250.0f;
-				src.m_left = CUT_ZERO + (floor(m_mat_num[0] / 10) * 125);
-				src.m_right = END_ZERO + (floor(m_mat_num[0] / 10) * 125);
-				src.m_bottom = 1375.0f;
+				////十の位
+				//src.m_top = 1250.0f;
+				//src.m_left = CUT_ZERO + (floor(m_mat_num[0] / 10) * 125);
+				//src.m_right = END_ZERO + (floor(m_mat_num[0] / 10) * 125);
+				//src.m_bottom = 1375.0f;
 
-				dst.m_top = FONT_Y + 150.0f;
-				dst.m_left = FONT_X + 340.0f;
-				dst.m_right = FONT_X + 360.0f;
-				dst.m_bottom = FONT_Y + 190.0f;
-				Draw::Draw(41, &src, &dst, c[4], 0.0f);
+				//dst.m_top = FONT_Y + 150.0f;
+				//dst.m_left = FONT_X + 340.0f;
+				//dst.m_right = FONT_X + 360.0f;
+				//dst.m_bottom = FONT_Y + 190.0f;
+				//Draw::Draw(41, &src, &dst, c[4], 0.0f);
 
-				//一の位
-				src.m_top = 1250.0f;
-				src.m_left = CUT_ZERO + ((m_mat_num[0] % 10) * 125);
-				src.m_right = END_ZERO + ((m_mat_num[0] % 10) * 125);
-				src.m_bottom = 1375.0f;
+				////一の位
+				//src.m_top = 1250.0f;
+				//src.m_left = CUT_ZERO + ((m_mat_num[0] % 10) * 125);
+				//src.m_right = END_ZERO + ((m_mat_num[0] % 10) * 125);
+				//src.m_bottom = 1375.0f;
 
-				dst.m_top = FONT_Y + 150.0f;
-				dst.m_left = FONT_X + 360.0f;
-				dst.m_right = FONT_X + 380.0f;
-				dst.m_bottom = FONT_Y + 190.0f;
-				Draw::Draw(41, &src, &dst, c[4], 0.0f);
+				//dst.m_top = FONT_Y + 150.0f;
+				//dst.m_left = FONT_X + 360.0f;
+				//dst.m_right = FONT_X + 380.0f;
+				//dst.m_bottom = FONT_Y + 190.0f;
+				//Draw::Draw(41, &src, &dst, c[4], 0.0f);
 				//swprintf_s(str, L"＋%d個", m_mat_num[0]);
+				FontDraw(NumConversion(m_mat_num[0]), FONT_X + 340, FONT_Y + 150, 20, 40, c[4], false);
 				//Font::StrDraw(str, FONT_X + 300, FONT_Y + 150, 40, c[4]);		//資材数A表示
 			}
 			//^------------------------------------------------------------------------
@@ -971,30 +972,31 @@ void CObjFightClear::Draw()
 				dst.m_bottom = FONT_Y + 240.0f;
 				Draw::Draw(41, &src, &dst, c[4], 0.0f);
 
-				//十の位
-				src.m_top = 1250.0f;
-				src.m_left =  CUT_ZERO + (floor(m_mat_num[1] / 10) * 125);
-				src.m_right =  END_ZERO + (floor(m_mat_num[1] / 10) * 125);
-				src.m_bottom = 1375.0f;
+				////十の位
+				//src.m_top = 1250.0f;
+				//src.m_left =  CUT_ZERO + (floor(m_mat_num[1] / 10) * 125);
+				//src.m_right =  END_ZERO + (floor(m_mat_num[1] / 10) * 125);
+				//src.m_bottom = 1375.0f;
 
-				dst.m_top = FONT_Y + 200.0f;
-				dst.m_left = FONT_X + 340.0f;
-				dst.m_right = FONT_X + 360.0f;
-				dst.m_bottom = FONT_Y + 240.0f;
-				Draw::Draw(41, &src, &dst, c[4], 0.0f);
+				//dst.m_top = FONT_Y + 200.0f;
+				//dst.m_left = FONT_X + 340.0f;
+				//dst.m_right = FONT_X + 360.0f;
+				//dst.m_bottom = FONT_Y + 240.0f;
+				//Draw::Draw(41, &src, &dst, c[4], 0.0f);
 
-				//一の位
-				src.m_top = 1250.0f;
-				src.m_left =  CUT_ZERO + ((m_mat_num[1] % 10) * 125);
-				src.m_right =  END_ZERO + ((m_mat_num[1] % 10) * 125);
-				src.m_bottom = 1375.0f;
+				////一の位
+				//src.m_top = 1250.0f;
+				//src.m_left =  CUT_ZERO + ((m_mat_num[1] % 10) * 125);
+				//src.m_right =  END_ZERO + ((m_mat_num[1] % 10) * 125);
+				//src.m_bottom = 1375.0f;
 
-				dst.m_top = FONT_Y + 200.0f;
-				dst.m_left = FONT_X + 360.0f;
-				dst.m_right = FONT_X + 380.0f;
-				dst.m_bottom = FONT_Y + 240.0f;
-				Draw::Draw(41, &src, &dst, c[4], 0.0f);
+				//dst.m_top = FONT_Y + 200.0f;
+				//dst.m_left = FONT_X + 360.0f;
+				//dst.m_right = FONT_X + 380.0f;
+				//dst.m_bottom = FONT_Y + 240.0f;
+				//Draw::Draw(41, &src, &dst, c[4], 0.0f);
 				//swprintf_s(str, L"＋%d個", m_mat_num[1]);
+				FontDraw(NumConversion(m_mat_num[1]), FONT_X + 340, FONT_Y + 150, 20, 40, c[4], false);
 				//Font::StrDraw(str, FONT_X + 300, FONT_Y + 200, 40, c[4]);	//資材数B表示
 			}
 
